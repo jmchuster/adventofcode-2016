28324dd30355ec11833e5976a4764b4d5442c228
Jonathan Chu <jmchuster@gmail.com>
2016-12-01 07:52:14 -0800

    basic cleanup

--- day1/day1.rb
+++ day1/day1.rb
@@ -12,26 +12,26 @@ def distance(instructions_string)
   orientation = N
   instructions.each do |instruction|
     turn = instruction[0]
-    distance = instruction[1..-1]
+    distance = instruction[1..-1].to_i
 
     orientation = case turn
-      when 'L' then (orientation - 1 + 4) % 4
-      when 'R' then (orientation + 1 + 4) % 4
+      when 'L' then (orientation - 1) % 4
+      when 'R' then (orientation + 1) % 4
     end
 
     case orientation
-      when N then y_distance += distance.to_i
-      when E then x_distance += distance.to_i
-      when S then y_distance -= distance.to_i
-      when W then x_distance -= distance.to_i
+      when N then y_distance += distance
+      when E then x_distance += distance
+      when S then y_distance -= distance
+      when W then x_distance -= distance
     end
   end
 
   return x_distance.abs + y_distance.abs
 end
 
-if __FILE__ == $0
-  input = File.read(ARGV[0])
+if __FILE__ == $0 || __FILE__ == '(pry)'
+  input = File.read('input.txt')
   inputs = input.split("\n").map(&:strip).reject(&:empty?)
   inputs.map{|input| puts distance(input) }
 end
--- day1/test.rb
+++ day1/test.rb
@@ -6,5 +6,3 @@ require './day1.rb'
 assert distance('R2, L3') == 5
 assert distance('R2, R2, R2') == 2
 assert distance('R5, L5, R5, R3') == 12
-
-puts 'OK'
